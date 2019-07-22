8349ae1a8f2f4ea7171ba2cacd8bad45e9824802
Armando Roman <armandoroman1016@gmail.com>
2019-07-22 13:00:50 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => (
   <div>
-    <Navigation />
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path = '/' exact component = {Home} />
+        <Route path = '/about' component = {About} />
+        <Route path = '/contact' component = {Contact} />
+      </Switch>
+    </Router>
   </div>
 );
 
